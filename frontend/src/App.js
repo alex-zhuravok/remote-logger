3f6297360191823ac006e13297e35930cf6a41cf
alex-zhuravok <8786795@gmail.com>
2018-10-18 17:53:54 +0300

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import LogTable from './components/LogTable'
 import logo from './logo.svg';
--- frontend/src/components/LogTable.js
+++ frontend/src/components/LogTable.js
@@ -13,24 +13,28 @@ class LogTable extends Component {
 
     render() {
         return (
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>Date and time</th>
-                    <th>Event type</th>
-                    <th>Message</th>
-                    <th>Additional data</th>
-                </tr>
-                {
-                    this.state.logs.map(logItem =>
-                        <tr key={logItem._id}>
-                            <td>{logItem.id}</td>
-                            <td>{logItem.moment}</td>
-                            <td>{logItem.type}</td>
-                            <td>{logItem.message}</td>
-                            <td></td>
-                        </tr>
-                )}
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Date and time</th>
+                        <th>Event type</th>
+                        <th>Message</th>
+                        <th>Additional data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.logs.map(logItem =>
+                            <tr key={logItem._id}>
+                                <td>{logItem.id}</td>
+                                <td>{logItem.moment}</td>
+                                <td>{logItem.type}</td>
+                                <td>{logItem.message}</td>
+                                <td></td>
+                            </tr>
+                    )}
+                </tbody>
             </table>
         );
     }
